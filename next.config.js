89504d14e5bede68001dd777f723258b7c6d433f
Pedro Carreño <pkcarrenodev@gmail.com>
2023-01-14 01:27:32 -0400

    fix: temp disable Next.js image optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+}
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: assetPrefix,
+  images: {
+    unoptimized: true,
+  }
 };
 
 const intercept = require("intercept-stdout");
