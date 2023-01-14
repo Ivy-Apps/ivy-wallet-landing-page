9954714800c5ba11555d2587177f508dea48dc97
Pedro Carreño <pkcarrenodev@gmail.com>
2023-01-14 00:30:47 -0400

    feat(navbar): navbar as individual component and styles

---
+++ components/navbar/index.jsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./navbar.module.scss";
+import RightArrow from "../../state/icons/rightArrow"
+
+const Logo = () => {
+  return(
+    <div className={styles.logo}>
+      <div>
+        <Link href="/">
+          <a>
+            <Image src="/logo.svg" alt="Ivy Logo" width="60" height="60" />
+          </a>
+        </Link>
+        <span className={styles.beta_span}>Beta</span>
+      </div>
+    </div>
+  )
+}
+
+const LinksBar = ({ links, titleToLinkMap }) => {
+  return(
+    <div className={styles.links}>
+      {links.map((e) => (
+        <Link href={titleToLinkMap[e]} key={e}>
+          <a>{e}</a>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+const ActionButtons = () => {
+  return(
+    <div className={styles.cta}>
+      <button>Donate</button>
+        <a href="https://play.google.com/store/apps/details?id=com.ivy.wallet&hl=en&gl=US" target="_blank" rel="noreferrer">
+          <button>
+            Try Ivy Wallet{" "}
+            <RightArrow />
+        </button>
+      </a>
+    </div>
+  )
+}
+
+export const Navbar = ({ links = [], titleToLinkMap = [] }) => {
+  return(
+      <header className={styles.container}>
+        <Logo />
+        <LinksBar links={links} titleToLinkMap={titleToLinkMap} />
+        <ActionButtons />
+      </header>
+  )
+};
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,9 @@
-import Link from "next/link";
-import Image from "next/image";
+import { Navbar } from "../components/navbar";
 import styles from "../styles/index.module.scss";
 import Head from "next/head";
 import dynamic from "next/dynamic";
 import { useRecoilValue } from "recoil";
 import { iconsState } from "../state";
-
-
 
 const World = dynamic(import("../components/world"), {
   ssr: false,
@@ -61,63 +58,13 @@ const Index = () => {
         <link rel="icon" href="/icon.png" />
       </Head>
 
-      <header>
-        <div className={styles.logo}>
-          <div>
-            <Link href="/">
-              <a>
-                <Image src="/logo.svg" alt="Ivy Logo" width="60" height="60" />
-              </a>
-            </Link>
-            <span className={styles.beta_span}>Beta</span>
-          </div>
-        </div>
-        <div className={styles.links}>
-          {links.map((e) => (
-            <Link href={titleToLinkMap[e]} key={e}>
-              <a>{e}</a>
-            </Link>
-          ))}
-        </div>
-        <div className={styles.cta}>
-          <button>
-            <a href="https://github.com/sponsors/Ivy-Apps" target="_blank">
-              Donate
-            </a>
-          </button>
-
-          <a
-            href="https://play.google.com/store/apps/details?id=com.ivy.wallet&hl=en&gl=US"
-            target="_blank"
-          >
-            <button>
-              Try Ivy Wallet{" "}
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  opacity="0.4"
-                  d="M8.20248 13.1903L4.50325 13.5175C3.67308 13.5175 3 12.8378 3 11.9996C3 11.1613 3.67308 10.4817 4.50325 10.4817L8.20248 10.8088C8.85375 10.8088 9.38174 11.3419 9.38174 11.9996C9.38174 12.6583 8.85375 13.1903 8.20248 13.1903Z"
-                  fill="currentColor"
-                />
-                <path
-                  d="M20.6247 13.1302C20.5669 13.1885 20.3509 13.4353 20.148 13.6402C18.9644 14.9234 15.8739 17.0218 14.2572 17.664C14.0117 17.7665 13.391 17.9846 13.0583 18C12.7408 18 12.4375 17.9262 12.1485 17.7808C11.7874 17.577 11.4994 17.2554 11.3401 16.8764C11.2387 16.6143 11.0794 15.8267 11.0794 15.8124C10.9212 14.9521 10.835 13.5531 10.835 12.0066C10.835 10.535 10.9212 9.19332 11.051 8.31871C11.0652 8.30329 11.2245 7.32623 11.3979 6.99137C11.7154 6.37892 12.3361 6 13.0005 6H13.0583C13.4914 6.01432 14.4012 6.39435 14.4012 6.40756C15.9317 7.04975 18.9491 9.04681 20.1622 10.3742C20.1622 10.3742 20.5047 10.7156 20.6531 10.9282C20.8844 11.2344 21 11.6134 21 11.9923C21 12.4153 20.8702 12.8085 20.6247 13.1302Z"
-                  fill="currentColor"
-                />
-              </svg>
-            </button>
-          </a>
-        </div>
-      </header>
+      <Navbar links={links} titleToLinkMap={titleToLinkMap} />
 
       <main className={styles.main}>
         <div className={styles.product}>
           <div className={styles.news}>
-            <a href="https://t.me/+ETavgioAvWg4NThk" target="_blank">
+            <a href="https://t.me/+ETavgioAvWg4NThk" target="_blank" rel="noreferrer">
+
               Join our telegram communinty for updates
               <svg
                 width="24"
@@ -147,6 +94,7 @@ const Index = () => {
             <a
               href="https://play.google.com/store/apps/details?id=com.ivy.wallet&hl=en&gl=US"
               target="_blank"
+              rel="noreferrer"
             >
               <button>
                 Try Ivy Wallet{" "}
