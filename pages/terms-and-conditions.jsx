37ea0cadeeb3501660c62a566f96b353be45dd4a
Pedro Carreño <pkcarrenodev@gmail.com>
2023-01-15 01:25:57 -0400

    feat(page): new terms and condition page

---
+++ pages/terms-and-conditions.jsx
@@ -0,0 +1,29 @@
+import { Container } from "../components/container";
+import styles from "../styles/terms.module.scss";
+
+const Terms = () => {
+  return(
+    <Container>
+      <div className={styles.content_wrapper}>
+        <h2>Terms & Conditions</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Leo integer malesuada nunc vel risus commodo. Quis imperdiet massa tincidunt nunc pulvinar sapien. Quam adipiscing vitae proin sagittis nisl. Ullamcorper a lacus vestibulum sed arcu non. Odio ut sem nulla pharetra. Ut pharetra sit amet aliquam id diam maecenas ultricies mi. Habitasse platea dictumst quisque sagittis purus sit amet volutpat consequat. Quis risus sed vulputate odio ut enim blandit volutpat. Ut placerat orci nulla pellentesque. Cursus sit amet dictum sit amet justo donec enim.
+        </p>
+        <p>
+          Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Bibendum at varius vel pharetra vel turpis nunc. Massa vitae tortor condimentum lacinia quis vel eros. Ut porttitor leo a diam sollicitudin tempor id eu nisl. In dictum non consectetur a. Ipsum suspendisse ultrices gravida dictum fusce ut placerat orci nulla. Consectetur libero id faucibus nisl tincidunt eget. Magna eget est lorem ipsum dolor sit amet consectetur. Ultricies leo integer malesuada nunc vel risus commodo viverra. Arcu risus quis varius quam. Orci sagittis eu volutpat odio facilisis. Vivamus at augue eget arcu dictum varius duis. Et malesuada fames ac turpis egestas sed. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien et ligula. Eu augue ut lectus arcu bibendum. Cras semper auctor neque vitae tempus quam pellentesque nec. Laoreet sit amet cursus sit amet. Pellentesque dignissim enim sit amet venenatis. Velit dignissim sodales ut eu sem integer vitae. Quam elementum pulvinar etiam non.
+        </p>
+        <p>
+          Ullamcorper malesuada proin libero nunc. Dictum at tempor commodo ullamcorper a lacus vestibulum. Feugiat sed lectus vestibulum mattis ullamcorper velit sed. Quam quisque id diam vel. Amet luctus venenatis lectus magna fringilla urna. Natoque penatibus et magnis dis parturient montes nascetur. Euismod quis viverra nibh cras pulvinar mattis nunc sed. Purus sit amet luctus venenatis lectus magna fringilla. Turpis massa sed elementum tempus. Mauris pharetra et ultrices neque ornare aenean.
+        </p>
+        <p>
+          Etiam dignissim diam quis enim lobortis scelerisque. Libero justo laoreet sit amet cursus. Convallis aenean et tortor at risus viverra adipiscing at in. Nulla facilisi cras fermentum odio eu feugiat pretium nibh ipsum. Cursus eget nunc scelerisque viverra mauris in aliquam sem. Dui id ornare arcu odio ut sem nulla pharetra diam. Ut placerat orci nulla pellentesque dignissim. Mattis aliquam faucibus purus in. Posuere ac ut consequat semper viverra nam libero justo laoreet. Iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui. Sem integer vitae justo eget magna fermentum. At varius vel pharetra vel turpis nunc. Felis bibendum ut tristique et egestas quis ipsum suspendisse. Neque ornare aenean euismod elementum. Lorem mollis aliquam ut porttitor leo a. Odio euismod lacinia at quis risus sed vulputate odio ut. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat in. Magna etiam tempor orci eu.
+        </p>
+        <p>
+          Sollicitudin aliquam ultrices sagittis orci a scelerisque purus. Id faucibus nisl tincidunt eget nullam non nisi est sit. Eu non diam phasellus vestibulum. Orci porta non pulvinar neque laoreet suspendisse. Mattis molestie a iaculis at erat. Dui sapien eget mi proin sed libero enim sed. Mi sit amet mauris commodo quis. Orci sagittis eu volutpat odio facilisis mauris. Orci eu lobortis elementum nibh tellus. In nulla posuere sollicitudin aliquam ultrices sagittis orci. Metus aliquam eleifend mi in. Amet mattis vulputate enim nulla aliquet porttitor lacus luctus.
+        </p>
+      </div>
+    </Container>
+  )
+}
+
+export default Terms;
