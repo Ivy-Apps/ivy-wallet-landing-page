485232961d8316e1a31918f2399bed90f5e3c09e
Pedro Carreño <pkcarrenodev@gmail.com>
2023-01-15 01:25:24 -0400

    feat(page): new 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,20 @@
+import { Container } from "../components/container";
+import styles from "../styles/404.module.scss";
+import Link from "next/link";
+
+const Error = () => {
+  return(
+    <Container>
+      <div className={styles.content_wrapper}>
+        <h2>This page does not exist</h2>  
+        <Link href="/">
+          <button className={styles.btn}>
+            Back to Home
+          </button>
+        </Link>
+      </div>
+    </Container>
+  )
+}
+
+export default Error;
