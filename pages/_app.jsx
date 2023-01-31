f1ba6ac0848d34a45dd256f82156a9f92a406712
Pedro Carreño <pkcarrenodev@gmail.com>
2023-01-31 17:30:59 -0400

    fix(icons): remove old icons workflow and deps

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,17 +1,16 @@
 import { Navbar } from "../components/navbar";
 import { Footer } from "../components/footer";
-import { RecoilRoot } from "recoil";
 
 import "../styles/globals.scss";
 import "remixicon/fonts/remixicon.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <RecoilRoot>
+    <>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
-    </RecoilRoot>
+    </>
   );
 }
 
--- state/icons/index.js
+++
@@ -1,7 +0,0 @@
-import chart_icon from "./chart";
-import lock_icon from "./lock";
-import users_icon from "./users";
-import wallet_icon from "./wallet";
-import right_arrow_icon from "./rightArrow";
-
-export { chart_icon, lock_icon, users_icon, wallet_icon, right_arrow_icon };
