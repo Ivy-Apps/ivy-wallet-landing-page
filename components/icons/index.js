a5a74de98d0115b7444a2c0caf901babb72dc687
Pedro Carreño <pkcarrenodev@gmail.com>
2023-01-31 17:11:50 -0400

    feat(icon): new icons usage workflow

---
+++ components/icons/index.js
@@ -0,0 +1,13 @@
+import ChartIcon from "./chart";
+import LockIcon from "./lock";
+import UsersIcon from "./users";
+import WalletIcon from "./wallet";
+import RightArrowIcon from "./rightArrow";
+
+export {
+  ChartIcon,
+  LockIcon,
+  UsersIcon,
+  WalletIcon,
+  RightArrowIcon
+};
